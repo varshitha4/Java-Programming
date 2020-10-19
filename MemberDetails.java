3d17d5f61ea206614fb81b8c2dd3c79ea5ce3f17
varshitha4 <72927203+varshitha4@users.noreply.github.com>
2020-10-19 18:48:54 +0530

    Add files via upload
    listing the details of the members

---
+++ MemberDetails.java
@@ -0,0 +1,43 @@
+class MemberDetails{
+	public static void main(String[] args){
+		Details kavya= new Details();
+		kavya.name= "kavya";
+		kavya.place= "Tarikere";
+		kavya.college= "Ait";
+		kavya.hobbies= "Drawing";
+		kavya.DOB= "09/04/1998";
+		kavya.ListingDetails();
+		
+		Details usha= new Details();
+		usha.name= "usha";
+		usha.place= "Davangere";
+		usha.college= "Ait";
+		usha.hobbies= "playing";
+		usha.DOB= "08/09/1998";
+		usha.ListingDetails();
+		
+		Details tasmiya= new Details();
+		tasmiya.name= "tasmiya";
+		tasmiya.place= "chikkamagaluru";
+		tasmiya.college= "Ait";
+		tasmiya.hobbies= "reading";
+		tasmiya.DOB= "10/01/1998";
+		tasmiya.ListingDetails();
+
+		Details ramya= new Details();
+		ramya.name= "ramya";
+		ramya.place= "chikkamagaluru";
+		ramya.college= "Ait";
+		ramya.hobbies= "drawing";
+		ramya.DOB= "24/08/1998";
+		ramya.ListingDetails();
+
+		Details srinivas= new Details();
+		srinivas.name= "srinivas";
+		srinivas.place= "jamkhandi";
+		srinivas.college= "BGMIT";
+		srinivas.hobbies= "playing";
+		srinivas.DOB= "15/03/1998";
+		srinivas.ListingDetails();
+	}
+}
