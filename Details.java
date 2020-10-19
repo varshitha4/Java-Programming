8772c71dabf0c08c71969ec662a50b3453627df8
varshitha4 <72927203+varshitha4@users.noreply.github.com>
2020-10-19 18:47:47 +0530

    Add files via upload
    to print the details of the group members

---
+++ Details.java
@@ -0,0 +1,15 @@
+class Details{
+	String name;
+	String place;
+	String college;
+	String hobbies;
+	String DOB;
+
+	void ListingDetails(){
+	System.out.println("name:" + " " + name);
+	System.out.println("place:" + " " + place);
+	System.out.println("college:" + " " +college);
+	System.out.println("Hobbies:" + " " +hobbies);
+	System.out.println("Date of birth:" + " " +DOB);
+	}
+}
